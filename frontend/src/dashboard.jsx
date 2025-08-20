a4fcb1a00979beab64a495d4c91a3892ec279060
Kevin Penner <crazyapple48@gmail.com>
2025-08-20 07:59:25 -0700

    Get buttons functioning to play playlists

--- frontend/src/dashboard.jsx
+++ frontend/src/dashboard.jsx
@@ -1,55 +1,85 @@
-import { Button, Container } from 'react-bootstrap';
-import { useGetPlaylists, useSpotifyToken } from './authentication/auth';
+import { Button, Container } from "react-bootstrap";
+import { useGetPlaylists, useSpotifyToken } from "./authentication/auth";
+import axios from "axios";
 
 function Dashboard() {
-  
-    
-    const tokenQuery = useSpotifyToken();
+  const tokenQuery = useSpotifyToken();
 
-    if (tokenQuery.isError) {
-        return <span>Error: {tokenQuery.error.message}</span>
+  if (tokenQuery.isError) {
+    return <span>Error: {tokenQuery.error.message}</span>;
+  }
+
+  const {
+    isPending: playlistPending,
+    isError: playlistIsError,
+    data: playlistData,
+    error: playlistError,
+  } = useGetPlaylists();
+
+  if (playlistPending) {
+    return <span>Loading....</span>;
+  }
+
+  if (playlistIsError) {
+    return <span>Error: {playlistError.message}</span>;
+  }
+
+  const playlists = playlistData.data.items;
+
+  const filteredPlaylists = playlists.filter((value, index, arr) => {
+    switch (value.name) {
+      case "Wedding Jams":
+        return true;
+      case "Cocktail hour":
+        return true;
+      case "Wedding Jams 2":
+        return true;
+      case "Dessert Jams":
+        return true;
+      case "Dinner":
+        return true;
+      case "They Kiss!":
+        return true;
+      case "Set-up":
+        return true;
+      case "Pre-Ceremony":
+        return true;
     }
+  });
 
-    const { isPending: playlistPending, isError: playlistIsError, data: playlistData,  error: playlistError } = useGetPlaylists()
-
-    if (playlistPending) {
-        return <span>Loading....</span>
-    }
-
-    if (playlistIsError) {
-        return <span>Error: {playlistError.message}</span>
-    }
-
-    const playlists = playlistData.data.items
-
-    const filteredPlaylists = playlists.filter((value, index, arr) => {
-        switch (value.name) {
-            case "Wedding Jams":
-                return true;
-            case "Cocktail hour":
-                return true;
-            case "Wedding Jams 2":
-                return true;
-            case "Dessert Jams":
-                return true;
-            case "Dinner":
-                return true;
-            case "They Kiss!":
-                return true;
-            case "Set-up":
-                return true;
-            case "Pre-Ceremony":
-                return true;
-        }
-    })
-    
-    return (
-        <Container className="d-flex flex-column w-50 h-100">
-            {filteredPlaylists.map((playlist) => {
-                return <Button key={playlist.href} variant='outline-success' >{playlist.name}</Button>
-            })}
-        </Container>
-    )
+  return (
+    <Container className="d-flex flex-column w-50 h-100">
+      {filteredPlaylists.map((playlist) => {
+        return (
+          <Button
+            key={playlist.href}
+            variant="outline-success"
+            onClick={() => playPlaylist(playlist.uri, tokenQuery.data.accessToken)}
+          >
+            {playlist.name}
+          </Button>
+        );
+      })}
+    </Container>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
+
+async function playPlaylist(href, accessToken) {
+  const endpoint = "https://api.spotify.com/v1/me/player/play";
+
+  const response = await axios.put(
+    (`${endpoint}`),
+    ({
+      context_uri: href,
+    }),
+    ({
+      headers: {
+        Authorization: "Bearer " + accessToken,
+      },
+    })
+  );
+
+  return response;
+}
