bfbac5806fc6b36ba3ed8376589e9d6bdcd12cd9
Kevin Penner <crazyapple48@gmail.com>
2025-08-21 08:02:55 -0700

    Fetch devices and play playlists. Something is still buggy playing to my phone though.

--- frontend/src/authentication/auth.js
+++ frontend/src/authentication/auth.js
@@ -1,106 +1,127 @@
 import { useQueryClient, useQuery, useMutation } from "@tanstack/react-query";
-import axios from 'axios'
-
+import axios from "axios";
 
 export function useLogin(code) {
-        const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-        return  useMutation({
-        mutationFn: async () => {
-            const result = await axios.post("https://192.168.0.39:3000/login", {
-            code,
-             })
-            return result
-        },
-        onSuccess: (response) => {
-            const tokenData = response.data;
+  return useMutation({
+    mutationFn: async () => {
+      const result = await axios.post("https://192.168.0.39:3000/login", {
+        code,
+      });
+      return result;
+    },
+    onSuccess: (response) => {
+      const tokenData = response.data;
 
-            const token = {
-                accessToken: tokenData.accessToken,
-                refreshToken: tokenData.refreshToken,
-                expiresAt: Date.now() + (tokenData.expiresIn * 1000)
-            }
-            queryClient.setQueryData(["spotify-token"], token);
+      const token = {
+        accessToken: tokenData.accessToken,
+        refreshToken: tokenData.refreshToken,
+        expiresAt: Date.now() + tokenData.expiresIn * 1000,
+      };
+      queryClient.setQueryData(["spotify-token"], token);
 
-            localStorage.setItem("spotify-token", JSON.stringify(token))
+      localStorage.setItem("spotify-token", JSON.stringify(token));
 
-            window.history.replaceState({}, document.title, "/")
-        },
-        onError: (error) => {
-            console.error(error);
-            // window.location.href = "/login"
-        }
-    })
-    
+      window.history.replaceState({}, document.title, "/");
+    },
+    onError: (error) => {
+      console.error(error);
+      // window.location.href = "/login"
+    },
+  });
+}
+
+export function useSpotifyToken(refreshToken) {
+  const queryClient = useQueryClient();
+
+  const cached = queryClient.getQueryData(["spotify-token"]);
+
+  const token = cached?.refreshToken;
+
+  return useQuery({
+    queryKey: ["spotify-token"],
+    queryFn: async () => {
+      if (!token) {
+        // window.location.href = "/login"
+        return null;
+      }
+
+      const { data } = await axios.post("https://192.168.0.39:3000/refresh", {
+        refreshToken: token,
+      });
+
+      return {
+        accessToken: data.accessToken,
+        refreshToken: token,
+        expiresAt: Date.now() + data.expiresIn * 1000,
+      };
+    },
+    onSuccess: (newToken) => {
+      queryClient.setQueryData(["spotify-token"], newToken);
+      localStorage.setItem("spotify-token", JSON.stringify(newToken));
+    },
+    enabled: !!token,
+    refetchInterval: (data) => {
+      if (!data) {
+        return false;
+      }
+
+      const msUntilExpiry = data.expiresAt - Date.now();
+
+      return msUntilExpiry > 60000 ? msUntilExpiry - 60000 : 0;
+    },
+    refetchIntervalInBackground: true,
+    onError: (err) => {
+      console.error(err);
+      window.location.href = "/login"
+    },
+  });
 }
 
 export function useGetPlaylists() {
+  const queryClient = useQueryClient();
+
+  const { data } = useSpotifyToken();
+
+  const accessToken = data?.accessToken;
+
+  return useQuery({
+    queryKey: ["playlists"],
+    queryFn: async () => {
+      const result = await axios.get(
+        "https://api.spotify.com/v1/users/crazyapple48/playlists",
+        {
+          headers: {
+            Authorization: "Bearer " + accessToken,
+          },
+        }
+      );
+      return result;
+    },
+    enabled: !!accessToken,
+  });
+}
+
+export function useGetDevices() {
     const queryClient = useQueryClient();
 
     const { data } = useSpotifyToken();
 
-    const  accessToken = data?.accessToken
-
-    return useQuery({ 
-        queryKey: ['playlists'], 
-        queryFn: async () => {
-
-            const result = await axios.get("https://api.spotify.com/v1/users/crazyapple48/playlists", { headers: {
-                                                            'Authorization': 'Bearer ' + accessToken,
-                                                        }})
-            return result;
-        },
-        enabled: !!accessToken
-    })
-
-}
-
-// the "smart" refresh flow
-
-export function useSpotifyToken(refreshToken) {
-    const queryClient = useQueryClient()
-
-    const cached = queryClient.getQueryData(["spotify-token"])
-
-    const token = cached?.refreshToken
+    const accessToken = data?.accessToken;
 
     return useQuery({
-        queryKey: ["spotify-token"],
+        queryKey: ["devices"],
         queryFn: async () => {
-
-            if (!token) {
-                // window.location.href = "/login"
-                return null
-            }
-
-            const { data } = await axios.post("https://192.168.0.39:3000/refresh", {
-                refreshToken: token
+            const result = await axios.get("https://api.spotify.com/v1/me/player/devices", {
+                headers: {
+                    Authorization: "Bearer " + accessToken
+                }
             });
-
-            return {
-                accessToken: data.accessToken,
-                refreshToken: token,
-                expiresAt: Date.now() + (data.expiresIn * 1000)
-            };
+            return result
         },
-        onSuccess: (newToken) => {
-            queryClient.setQueryData(["spotify-token"], newToken);
-            localStorage.setItem("spotify-token", JSON.stringify(newToken))
-        },
-        enabled: !!token,
-        refetchInterval: (data) => {
-            if (!data) {
-                return false;
-            }
-
-            const msUntilExpiry = data.expiresAt - Date.now();
-
-            return msUntilExpiry > 60000 ? msUntilExpiry - 60000 : 0;
-        },
-        refetchIntervalInBackground: true,
-        onError: (err) => {
-            console.error(err)
-            // window.location.href = "/login"
-        }
+        enabled: !!accessToken,
+        refetchInterval: 1000 * 5,
+        refetchIntervalInBackground: true
     })
 }
--- frontend/src/dashboard.jsx
+++ frontend/src/dashboard.jsx
@@ -1,12 +1,26 @@
-import { useGetPlaylists, useSpotifyToken } from "./authentication/auth";
+import { useState } from "react";
+import {
+  useGetDevices,
+  useGetPlaylists,
+  useSpotifyToken,
+} from "./authentication/auth";
 import axios from "axios";
 
 function Dashboard() {
   const tokenQuery = useSpotifyToken();
 
+  const [playbackDevice, setPlaybackDevice ] = useState();
+
   if (tokenQuery.isError) {
     return <span>Error: {tokenQuery.error.message}</span>;
   }
+
+  const {
+    isPending: devicesPending,
+    isError: devicesIsError,
+    data: devicesData,
+    error: devicesError,
+  } = useGetDevices();
 
   const {
     isPending: playlistPending,
@@ -15,17 +29,22 @@ function Dashboard() {
     error: playlistError,
   } = useGetPlaylists();
 
-  if (playlistPending) {
-    return <span>Loading....</span>;
-  }
-
   if (playlistIsError) {
     return <span>Error: {playlistError.message}</span>;
   }
 
-  const playlists = playlistData.data.items;
+  if (devicesIsError) {
+    return <span>Error: {devicesError.message}</span>
+  }
 
-  const filteredPlaylists = playlists.filter((value, index, arr) => {
+  const playlists = playlistData?.data?.items;
+  const devices = devicesData?.data?.devices;
+
+  if (!devices) {
+    return <span>Something went wrong</span>
+  }
+
+  const filteredPlaylists = playlists?.filter((value, index, arr) => {
     switch (value.name) {
       case "Wedding Jams":
         return true;
@@ -50,10 +69,15 @@ function Dashboard() {
     <>
       <div className="grid grid-cols-[2fr_1fr] items-center justify-center bg-black h-screen">
         <div className="col-2 row-2 justify-self-end self-start pr-3">
-          <select className="bg-[#8833ff] p-2 rounded-lg">
-            <option className="bg-[#8833ff] rounded-lg">My Device</option>
-            <option className="bg-[#8833ff] rounded-lg">My Device</option>
-          </select>
+          {devicesPending ? (
+            <span className="text-white">Loading....</span>
+          ) : (
+            <select className="bg-[#8833ff] p-2 rounded-lg" onChange={e => setPlaybackDevice(e.target.value)}>
+              {devices.map((device) => {
+                return <option key={device.id} value={device.id}>{device.name}</option>
+              })}
+            </select>
+          )}
         </div>
         <div className="col-1 row-1 col-span-2">
           <h1 className="text-3xl font-bold underline text-white justify-self-center">
@@ -61,19 +85,23 @@ function Dashboard() {
           </h1>
         </div>
         <div className="col-1 row-2 flex flex-col self-start justify-self-center">
-          {filteredPlaylists.map((playlist) => {
-            return (
-              <button
-                className="rounded-lg bg-[#aa33aa] p-3 my-1"
-                key={playlist.href}
-                onClick={() =>
-                  playPlaylist(playlist.uri, tokenQuery.data.accessToken)
-                }
-              >
-                {playlist.name}
-              </button>
-            );
-          })}
+          {playlistPending ? (
+            <span className="text-white">Loading...</span>
+          ) : (
+            filteredPlaylists.map((playlist) => {
+              return (
+                <button
+                  className="rounded-lg bg-[#aa33aa] p-3 my-1"
+                  key={playlist.href}
+                  onClick={() =>
+                    playPlaylist(playlist.uri, tokenQuery.data.accessToken, playbackDevice)
+                  }
+                >
+                  {playlist.name}
+                </button>
+              );
+            })
+          )}
         </div>
       </div>
     </>
@@ -82,8 +110,8 @@ function Dashboard() {
 
 export default Dashboard;
 
-async function playPlaylist(href, accessToken) {
-  const endpoint = "https://api.spotify.com/v1/me/player/play";
+async function playPlaylist(href, accessToken, device) {
+  const endpoint = `https://api.spotify.com/v1/me/player/play?device_id=${device}`;
 
   const response = await axios.put(
     `${endpoint}`,
