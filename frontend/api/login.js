93a3f8db2470ac8a3e67517f1afd9a306605323e
Kevin Penner <crazyapple48@gmail.com>
2025-09-02 22:02:58 -0700

    Fix dumb dumb

--- frontend/api/login.js
+++ frontend/api/login.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
         return res.status(405).json({ error: "Method not allowed" })
     }
 
-    const { code } = req.body.code
+    const { code } = req.body
     if (!code) {
         return res.status(400).json({ error: "Missing code"})
     }
