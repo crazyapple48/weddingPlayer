9846dc0d7d613c0f96b445601e96586f03916cb2
Kevin Penner <crazyapple48@gmail.com>
2025-09-02 21:16:22 -0700

    Add dev config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,8 @@
 import { defineConfig } from 'vite'
-import fs from 'fs'
-import path from 'path'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, './192.168.0.39+2-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, './192.168.0.39+2.pem'))
-    },
-    hmr: {
-      protocol: 'wss',
-      host: '192.168.0.39',
-      port: 5000
-    },
-    host: '0.0.0.0'
-  }
 })
